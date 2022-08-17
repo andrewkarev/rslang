d83dee6fe36af4d8cdd9b51cc53963a7dbdab5b4
Andrew <an.carev4@gmail.com>
2022-08-17 17:14:17 +0300

    refactor: add basic structure

---
+++ rslang/src/components/main-page/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './main-page.css';
+import Header from '../header/Header';
+
+const MainPage = () => {
+  return (
+    <div className="main-page">
+      <Header />
+    </div>
+  );
+};
+
+export default MainPage;
