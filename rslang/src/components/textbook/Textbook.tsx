82c1fddc4ac0914018367db2d38f18d8c6d90010
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-08-19 10:11:27 +0300

    feat: add TextBookCards component

--- rslang/src/components/textbook/Textbook.tsx
+++ rslang/src/components/textbook/Textbook.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../header/Header';
 import Levels from './levels/Levels';
 import './textbook.css';
+import TextbookCards from './textbook-cards/TextbookCards';
 
 const Textbook = () => {
   return (
@@ -11,6 +12,14 @@ const Textbook = () => {
         <div className="wrapper textbook-wrapper">
           <h2 className="title">Учебник</h2>
           <Levels />
+          <div className="book-wrapper a-level-group">
+            <h2 className="title">Слова</h2>
+            <div className="words-container">
+              <TextbookCards />
+              {/* <CurrentWord /> */}
+            </div>
+            {/* <Pagination /> */}
+          </div>
         </div>
       </div>
     </>
---
+++ rslang/src/components/textbook/textbook-cards/TextbookCards.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './textbook-cards.css';
+
+const TextbookCards = () => {
+  const cardsData = new Array(20).fill({ word: 'Word', translation: 'перевод' });
+  const cardsElements = cardsData.map((card, index) => {
+    return (
+      <div className="card">
+        <h3 className="word">{ card.word + (index + 1)}</h3> 
+        <p className="translation">{ card.translation + (index + 1)}</p>
+      </div>
+    )
+  });
+
+  return (
+    <div className="words-grid">
+      { cardsElements }
+    </div>
+  )
+}
+
+export default TextbookCards;
