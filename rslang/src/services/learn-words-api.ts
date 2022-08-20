d1991c803b071cebda99420fadb868eb9050506d
Andrew <an.carev4@gmail.com>
2022-08-20 17:16:02 +0300

    feat: implement `Learn-words-api` class for interaction with API

--- rslang/src/services/learn-words-api.ts
+++ rslang/src/services/learn-words-api.ts
@@ -1,14 +1,210 @@
-class LearnWordApi {
-  root: string;
-  token: string;
+import RequestError from '../types/errors/RequestError';
+import IRequestFunctionOptions from '../types/services-interfaces/IRequestFunction';
+import IUser from '../types/services-interfaces/IUser';
+import IUserToken from '../types/services-interfaces/IUserToken';
+import IUserWord from '../types/services-interfaces/IUserWord';
+import IWord from '../types/services-interfaces/IWord';
+
+class LearnWordsApi {
+  private root: string;
+  private token: string;
+  private refreshToken: string;
+  private wordsPath: string;
+  private usersPath: string;
 
   constructor() {
-    this.root = '<url можно взять из env variables например>';
-    this.token = localStorage.get('token')
+    this.root = 'https://rslangappteam102.herokuapp.com';
+    this.token = localStorage.getItem('token') || '';
+    this.refreshToken = localStorage.getItem('refreshToken') || '';
+    this.wordsPath = this.root + '/words';
+    this.usersPath = this.root + '/users';
   }
 
-  async getUser(id: string) {
+  private setTokensValue(token: string, refreshToken: string): void {
+    localStorage.setItem('token', token)
+    localStorage.setItem('refreshToken', refreshToken)
+    this.token = token;
+    this.refreshToken = refreshToken;
+  }
+
+  private async updateTokensValue(): Promise<void> {
+    const userId = localStorage.getItem('id') || '';
+    const response = await this.getNewUserTokens(userId);
+    const newToken = response ? response.token : '';
+    const newRefreshToken = response ? response.refreshToken : '';
+
+    this.setTokensValue(newToken, newRefreshToken);
+  }
+
+  private async createRequest<T>(url: string, options?: IRequestFunctionOptions): Promise<T | void> {
+    const errorStatuses = [400, 401, 403, 404, 417, 422];
+
+    try {
+      const response = options
+        ? await fetch(url, { ...options })
+        : await fetch(url);
+
+      if (response && errorStatuses.includes(response.status)) {
+        throw new RequestError(`${response.status}`);
+      }
+
+      if (response.status === 204) return;
+
+      const data = await response.json() as T;
+
+      return data;
+    } catch (error) {
+      if (!(error instanceof Error)) return;
+      if (!(error instanceof RequestError)) throw new Error(error.message);
+
+      if (error.message === '422' || error.message === '403') {
+        console.warn('Incorrect e-mail or password.')
+      };
+
+      if (error.message === '400') console.warn('Bad request.');
+
+      if (error.message === '417') {
+        const message = url.includes('words') ? 'word' : 'user';
+        console.warn(`A ${message} with the same name already exists.`);
+      };
+
+      if (error.message === '404') {
+        const message = url.includes('words') ? 'User\'s word' : 'User';
+        console.warn(`${message} not found.`);
+      };
+
+      if (error.message === '401') {
+        console.warn('Access token is missing or invalid.');
+        await this.updateTokensValue();
+
+        if (options && !options.headers) return;
+
+        const newOptions = { ...options };
+        const newAuthorizationHeader = { 'Authorization': `Bearer ${this.token}` };
+        newOptions.headers = { ...newOptions.headers, ...newAuthorizationHeader }
+
+        await this.createRequest(url, newOptions);
+      };
+    }
+  }
+
+  public async getWords(group = 0, page = 0): Promise<IWord[] | void> {
+    const queryURL = `${this.wordsPath}?page=${page}&group=${group}`;
+    return await this.createRequest(queryURL);
+  }
+
+  public async getWord(wordId: string): Promise<IWord | void> {
+    const queryURL = `${this.wordsPath}/${wordId}`;
+    return await this.createRequest(queryURL);
+  }
+
+  public async createUser(body: Pick<IUser, 'name' | 'email' | 'password'>): Promise<void> {
+    const stringifiedBody = JSON.stringify(body);
+
+    const response = await this.createRequest(this.usersPath, {
+      method: 'POST',
+      body: `${stringifiedBody}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    }) as Pick<IUser, 'name' | 'email' | 'id'> | void;
+
+    if (!response) return;
+
+    Object.entries(response).forEach(([key, value]) => {
+      localStorage.setItem(key, value);
+    });
+  }
+
+  public async getUser(id: string): Promise<Pick<IUser, 'name' | 'email' | 'id'> | void> {
+    const queryURL = `${this.usersPath}/${id}`;
+
+    return await this.createRequest(queryURL, {
+      headers: {
+        'Authorization': `Bearer ${this.token}`,
+      }
+    });
+  }
+
+  private async getNewUserTokens(id: string): Promise<IUserToken | void> {
+    const queryURL = `${this.usersPath}/${id}/tokens`;
+    return await this.createRequest(queryURL, {
+      headers: {
+        'Authorization': `Bearer ${this.refreshToken}`,
+      },
+    });
+  }
+
+  public async getUserWords(id: string): Promise<IUserWord[] | void> {
+    const queryURL = `${this.usersPath}/${id}/words`;
+    return await this.createRequest(queryURL, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${this.token}`,
+      },
+    });
+  }
+
+  public async createUserWord(id: string, wordId: string, body: IUserWord)
+    : Promise<IUserWord | void> {
+    const queryURL = `${this.usersPath}/${id}/words/${wordId}`;
+    const stringifiedBody = JSON.stringify(body);
+    return await this.createRequest(queryURL, {
+      method: 'POST',
+      body: `${stringifiedBody}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${this.token}`,
+      },
+    });
+  }
+
+  public async getUserWord(id: string, wordId: string): Promise<IUserWord | void> {
+    const queryURL = `${this.usersPath}/${id}/words/${wordId}`;
+    return await this.createRequest(queryURL, {
+      headers: {
+        'Authorization': `Bearer ${this.token}`,
+      },
+    });
+  }
+
+  public async updateUserWord(id: string, wordId: string, body: IUserWord)
+    : Promise<IUserWord | void> {
+    const queryURL = `${this.usersPath}/${id}/words/${wordId}`;
+    const stringifiedBody = JSON.stringify(body);
+    return await this.createRequest(queryURL, {
+      method: 'PUT',
+      body: `${stringifiedBody}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${this.token}`,
+      },
+    });
+  }
+
+  public async signIn(body: Pick<IUser, 'email' | 'password'>): Promise<void> {
+    const queryURL = `${this.root}/signin`;
+    const stringifiedBody = JSON.stringify(body);
+    const response = await this.createRequest(queryURL, {
+      method: 'POST',
+      body: `${stringifiedBody}`,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    }) as IUserToken | void;
+
+    if (!response) return;
+
+    const token = response.token;
+    const refreshToken = response.refreshToken;
+
+    this.setTokensValue(token, refreshToken);
   }
 }
 
-export default LearnWordApi;
+export default LearnWordsApi;
