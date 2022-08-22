f9313b46d0bf64afca059b9985989f4a2c7544bb
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-08-22 14:50:40 +0300

    refactor: modify cumbersome ternary operator to if operators chain

--- rslang/src/components/textbook/pagination/Pagination.tsx
+++ rslang/src/components/textbook/pagination/Pagination.tsx
@@ -6,15 +6,21 @@ const Pagination = () => {
   const currentPage = 1;
 
   const pagesData = new Array(7).fill(0).map((page, index) => {
-    return (
-    index === 0 ? index + 1 : 
-        index === 6 ? 30 : 
-        currentPage < 4 ? index < 5 ? index + 1 : '...' :
-        currentPage > MAX_PAGE - 4 ? index > 1 ? MAX_PAGE - (7 - index) + 1  : '...' : 
-        index === 2 ? currentPage - 1: 
-        index === 3 ? currentPage : 
-        index === 4 ? currentPage + 1 : '...'
-    )
+     
+      if (index === 0) return 1;
+      if (index === 6) return MAX_PAGE;
+
+      if (currentPage < 4 && index < 5) return index + 1;
+      if (currentPage < 4) return '...';
+
+      if (currentPage > MAX_PAGE - 4 && index > 1) return MAX_PAGE - (7 - index) + 1;
+      if (currentPage > MAX_PAGE - 4) return '...'
+
+      if (index === 2) return currentPage - 1;
+      if (index === 3) return currentPage;
+      if (index === 4) return currentPage + 1;
+
+      return '...';
   });
 
   const pageElements = pagesData.map((page, index) => {
