7d76c1649b8652f1596e7e35983168f4f0093bb0
Andrew <an.carev4@gmail.com>
2022-08-22 20:09:38 +0300

    feat: implement authorisation component

--- rslang/src/App.tsx
+++ rslang/src/App.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/main-page/MainPage';
 import Textbook from './components/textbook/Textbook';
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import AuthorisationForm from './components/header/authorisation/AuthorisationForm';
 
 function App() {
+  //temporary state
+  const [isModalOpened, setIsModalOpened] = useState(false);
+
+  const toggleModalVisability = () => {
+    setIsModalOpened(!isModalOpened);
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="textbook" element={<Textbook />} />
-    </Routes>
+    <>
+      <Header toggleModalVisability={toggleModalVisability} />
+      <Routes>
+        <Route path="/" element={
+          <>
+            <MainPage />
+            {/* <Footer /> */}
+          </>
+        } />
+        <Route path="textbook" element={<Textbook />} />
+      </Routes>
+      {isModalOpened && <AuthorisationForm toggleModalVisability={toggleModalVisability} />}
+    </>
   );
 }
 
