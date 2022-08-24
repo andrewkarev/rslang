34e565c122e5c8fc77c4629d618c8afdb8f9088b
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-08-24 14:23:23 +0300

    feat: render words data from database for selected level

--- rslang/src/components/textbook/textbook-cards/TextbookCards.tsx
+++ rslang/src/components/textbook/textbook-cards/TextbookCards.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import IWord from '../../../types/services-interfaces/IWord';
 import styles from './textbook-cards.module.css';
 
-const TextbookCards = () => {
-  const cardsData = new Array(20).fill({ word: 'Word', translation: 'перевод' });
+type Props = {
+  words: IWord[];
+}
+
+const TextbookCards = (props: Props) => {
+  const cardsData = new Array(props.words.length).fill({ word: 'Word', translation: 'перевод' });
 
   const cardsElements = cardsData.map((card, index) => {
     return (
       <div className={ styles['card'] } key={ index + 'card' }>
         <h3 className={ styles['word'] }>
-          { card.word + (index + 1) }
+          { props.words[index].word }
         </h3> 
         <p className={ styles['translation'] }>
-          { card.translation + (index + 1) }
+          { props.words[index].wordTranslate }
         </p>
       </div>
     )
   });
+
+  //console.log(props.words);
 
   return (
     <div className={ styles['words-grid'] }>
