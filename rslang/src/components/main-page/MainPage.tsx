2e6bd032c124bcda8e6f912382cd222f1142478d
RabykoSasha <rabykosasha@yandex.ru>
2022-09-01 22:57:41 +0300

    refactor: replace location of header and footer components

--- rslang/src/components/main-page/MainPage.tsx
+++ rslang/src/components/main-page/MainPage.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import styles from './main-page.module.css';
-import Header from '../header/Header';
 import HeroPages from './hero-page/HeroPage';
 import Advantagies from './advantagies/Advantagies';
 import Presentation from './presentation/Presentation';
 import DevTeam from './dev-team/DevTeam';
-import Footer from '../footer/Footer';
 import Scroll from './scroll-up/Scroll'
 
 const MainPage = () => {
   return (
     <div className={ styles["main-page"]}>
-      <Header />
       <HeroPages />
       <Advantagies />
       <Presentation />
       <DevTeam />
       <Scroll />
-      <Footer />
       </div>
   );
 };
