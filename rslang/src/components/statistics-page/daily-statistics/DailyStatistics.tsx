3a336ccbcc6af30757e2151e84a9ce32316487ef
Tatyana Kritskaya <kritskaya.tatjana@gmail.com>
2022-09-01 23:22:56 +0300

    feat: add styles for daily statistics components

--- rslang/src/components/statistics-page/daily-statistics/DailyStatistics.tsx
+++ rslang/src/components/statistics-page/daily-statistics/DailyStatistics.tsx
@@ -1,28 +1,103 @@
 import React from 'react';
 import styles from './daily-statistics.module.css';
 import WordStatistic from './word-statistic/WordStatistic';
+import idea from './../../../assets/icons/new.png';
+import report from './../../../assets/icons/report.png';
+import chart from './../../../assets/icons/chart.png';
+import streak from './../../../assets/icons/streak.png';
+import GameStatistic from './game-statistic/GameStatistic';
 
 const DailyStatistics = () => {
 
 	const wordsStatisticData = [
-		{ title: 'новых слов за день', value: '10' },
-    { title: 'изученных слов за день', value: '25' },
-    { title: 'правильных ответов за день', value: '80%' }
+		{ 
+			title: 'новых слов за день', 
+			value: '10', 
+			image:  idea
+		},
+    { 
+			title: 'изученных слов за день', 
+			value: '25', 
+			image: report
+		},
+    { 
+			title: 'правильных ответов за день', 
+			value: '80%', 
+			image: chart
+		}
 	];
 
-  const wordsStatisticsElements = wordsStatisticData.map((item) => {
+  const wordsStatisticsElements = wordsStatisticData.map((item, index) => {
     return (
-      <WordStatistic title={ item.title } value={ item.value }/>
+      <WordStatistic 
+				title={ item.title } 
+				value={ item.value }
+				image={ item.image }  
+				key={ `word-stat-${index}` }
+			/>
     )
   });
+
+	const gamesStatisticsData = [
+		{
+			name: 'Спринт',
+			stats: [
+				{
+					title: 'новых слов за день',
+					value: '4',
+					image: idea
+				},
+				{
+					title: 'процент правильных ответов',
+					value: '90%',
+					image: chart
+				},
+				{
+					title: 'самая длинная серия правильных ответов',
+					value: '15',
+					image: streak
+				}
+			]
+		},
+		{
+			name: 'Аудиовызов',
+			stats: [
+				{
+					title: 'новых слов за день',
+					value: '6',
+					image: idea
+				},
+				{
+					title: 'процент правильных ответов',
+					value: '92%',
+					image: chart
+				},
+				{
+					title: 'самая длинная серия правильных ответов',
+					value: '10',
+					image: streak
+				}
+			]
+		}
+	];
+
+	const gamesStatisticsElements = gamesStatisticsData.map((item, index) => {
+		return (
+			<GameStatistic 
+				name={ item.name } 
+				stats={ item.stats } 
+				key={ `game-stat-${index}` }
+			/>
+		)
+	})
 
 	return (
 		<div className={ styles['daily-statistics'] }>
 			<div className={ styles['words-statistics'] }>
         { wordsStatisticsElements }
 			</div>
-			<div className="games-statistics">
-
+			<div className={ styles['games-statistics'] }>
+				{ gamesStatisticsElements }
 			</div>
 		</div>
 	)
--- rslang/src/components/statistics-page/daily-statistics/word-statistic/WordStatistic.tsx
+++ rslang/src/components/statistics-page/daily-statistics/word-statistic/WordStatistic.tsx
@@ -4,13 +4,14 @@ import styles from './word-statistic.module.css';
 type Props = {
   title: string;
   value: string;
+  image: string;
 }
 
-const WordStatistic: React.FC<Props>= ({ title, value }) => {
+const WordStatistic: React.FC<Props>= ({ title, value, image }) => {
 	return (
 		<div className={ styles['word-statistic'] }>
       <p className={ styles['value'] }>{ value }</p>
-      <img src="" alt="" className="icon" />
+      <img className={ styles['icon'] } src={ image } alt="word stats" />
       <h3 className={ styles['title'] }>{ title }</h3>
     </div>
 	)
