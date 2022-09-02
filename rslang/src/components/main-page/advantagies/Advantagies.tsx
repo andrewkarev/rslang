258b348bd2ba0ac1dee21d6e1d93fb2d6d4c565b
RabykoSasha <rabykosasha@yandex.ru>
2022-09-02 15:21:55 +0300

    refactor: created data for Advantagies

--- rslang/src/components/main-page/advantagies/Advantagies.tsx
+++ rslang/src/components/main-page/advantagies/Advantagies.tsx
@@ -1,39 +1,11 @@
 import React from 'react';
 import styles from './advantagies.module.css';
-import textbook from './../../../assets/images/books.png';
-import padlock from './../../../assets/images/padlock.png';
-import diagram from './../../../assets/images/diagram.png';
-import trophy from './../../../assets/images/trophy.png';
+import advantagiesData from '../../../data/advantagies-data'
 import Advantagie from './advantagie/Advantagie';
 
 const Advantagies = () => {
-  const gamesData = [
-    { 
-      name: 'Учебник', 
-      description: 'Более 3500 тысяч слов для изучения, разбитых на разделы по уровню твоей подготовки с удобной навигацией.', 
-      image: textbook,
-      url: '/textbook',
-    },
-    { 
-      name: 'Регистрация', 
-      description: 'Открывает доступ к разделам "Сложные слова", прогресс изучения слов, изученные слова и статистика', 
-      image: padlock 
-    },
-    { 
-      name: 'Статистика', 
-      description: 'Отслеживайте статистику по мини-играм и по словам за каждый день изучения', 
-      image: diagram,
-      url: '/statistics',
-    },
-    { 
-      name: 'Игры', 
-      description: 'Спринт и Аудио-вызов - увлекательные игры на развитие запоминания слов, восприятия на слух и письма.', 
-      image: trophy,
-      url: '/games',
-    },
-  ];
 
-  const gameElements = gamesData.map((adventagie) => {
+  const advantagiesElement = advantagiesData.map((adventagie) => {
     return (
       <Advantagie image={ adventagie.image } name={ adventagie.name } description={ adventagie.description } url={ adventagie.url} />
     )
@@ -44,7 +16,7 @@ const Advantagies = () => {
       <div className={ styles['wrapper']}>
         <h2 className={ styles['title']}>Используйте все преимущества приложения</h2>
           <div className={ styles['content']}>
-            { gameElements }
+            { advantagiesElement }
           </div>        
       </div>
     </section>
