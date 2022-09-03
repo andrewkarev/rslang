b86432845c541366dfb26cbf480ba8fcd46405fc
Andrew <an.carev4@gmail.com>
2022-09-03 16:57:46 +0300

    feat: implement `IStatisti` interface

---
+++ rslang/src/types/services-interfaces/IStatistics.ts
@@ -0,0 +1,24 @@
+interface IStatistics {
+  optional: {
+    [date: string]: {
+      allAnswers: number,
+      rightAnswers: number,
+      learnedWords: number,
+      newWords: number,
+      games: {
+        sprint: {
+          newWords: number,
+          rightAnswers: number,
+          longestStreak: number,
+        },
+        audioCall: {
+          newWords: number,
+          rightAnswers: number,
+          longestStreak: number,
+        },
+      },
+    }[],
+  }
+};
+
+export default IStatistics;
