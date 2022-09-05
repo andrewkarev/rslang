737830251081fefe39d01adb975dc6f03c7d2f87
Andrew <an.carev4@gmail.com>
2022-09-05 16:38:06 +0300

    fix: improve burger menu animation

--- rslang/src/components/header/navigation/Navigation.tsx
+++ rslang/src/components/header/navigation/Navigation.tsx
@@ -1,26 +1,35 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
 import styles from './navigation.module.css';
 import NavList from "./navlist/NavList";
 
 const Navigation = () => {
-  
+
   const [opened, setOpened] = useState(false);
-  
+
   const toggleMenu = () => {
+    const body = document.querySelector('body');
+
+    if (!body) return;
+
+    if (!opened) {
+      body.style.overflow = 'hidden';
+    } else {
+      body.style.overflow = 'auto';
+    }
+
     setOpened(!opened);
   }
-  
+
   return (
-    <nav className={ styles["nav"] }>
-      <NavList isOpened={opened} toggleMenu={toggleMenu}/>
-      <div 
-        className={ styles["burger"] }
-        onClick={ toggleMenu }
+    <nav className={styles["nav"]}>
+      <NavList isOpened={opened} toggleMenu={toggleMenu} />
+      <div
+        className={styles["burger"]}
+        onClick={toggleMenu}
       >
-        <span className={ styles["burger-line"] }></span>
-        <span className={ styles["burger-line"] }></span>
-        <span className={ styles["burger-line"] }></span>
+        <span className={styles["burger-line"]}></span>
+        <span className={styles["burger-line"]}></span>
+        <span className={styles["burger-line"]}></span>
       </div>
     </nav>
   );
